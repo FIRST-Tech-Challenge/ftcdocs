c2758d5699c1272344a25b33bb842c58417d58a0
texasdiaz <texasdiaz@gmail.com>
2024-02-20 00:27:47 -0500

    Begone!

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -268,15 +268,6 @@ linkcheck_request_headers = {
         "Accept-Encoding": "gzip,deflate",
         "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
     },
-    "https://stackoverflow.com/": {
-        "Origin": "https://stackoverflow.com",
-        "Referer": "https://stackoverflow.com/",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-        "Accept-Language": "en-us,en;q=0.5",
-        "Accept-Encoding": "gzip,deflate",
-        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
-    },
     "*": {
         "Accept": "text/html,application/xhtml+xml",
     },
@@ -298,6 +289,7 @@ linkcheck_ignore = [
    r'https://www.autodesk.com/',
    r'https://knowledge.autodesk.com/',
    r'https://www.3dflow.net/',
+   r'https://stackoverflow.com',
 ]
 
 latex_documents = [
