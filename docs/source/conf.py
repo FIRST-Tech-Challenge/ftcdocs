9e3bebfadd538ec5c56dfd105cbd0e640cf31e89
texasdiaz <texasdiaz@gmail.com>
2024-02-20 00:03:46 -0500

    Linkfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -268,25 +268,6 @@ linkcheck_request_headers = {
         "Accept-Encoding": "gzip,deflate",
         "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
     },
-    "https://www.stackoverflow.com/": {
-        "Origin": "https://www.stackoverflow.com",
-        "Referer": "https://www.stackoverflow.com/",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
-        "Cache-Control": "no-cache",
-        "Pragma":"no-cache",
-        "Upgrade-Insecure-Requests":"1",
-        "Sec-Ch-Ua": "\"Not A(Brand\"\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": "\"Windows\"",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-    },
     "*": {
         "Accept": "text/html,application/xhtml+xml",
     },
@@ -308,6 +289,7 @@ linkcheck_ignore = [
    r'https://www.autodesk.com/',
    r'https://knowledge.autodesk.com/',
    r'https://www.3dflow.net/'
+   r'https://www.stackoverflow.com/'
 ]
 
 latex_documents = [
